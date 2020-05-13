0736ee00ba5d139e41ca99fdfe99f7867ead4c38
Nixon <nixon.emoony@gmail.com>
2020-05-13 22:17:04 +0100

    Run Nicely It now works for my notes

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,20 @@
 
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Command, Output};
 
 use anyhow::{anyhow, Context, Result};
+use regex::bytes::Regex;
 use structopt::StructOpt;
+use lazy_static::lazy_static;
 
 use log::{debug, error, info, trace, warn};
 
 //mod strings;
+//mod comp_db;
+//mod fls;
+//use crate::fls::FLSFile;
+
 
 #[derive(StructOpt, Debug)]
 struct Cli {
@@ -35,6 +41,7 @@ struct Cli {
 const LATEX_CMD: &str = "pdflatex";
 const OUTPUT_DIR: &str = "out";
 const TEX_FILE_EXT: &str = "tex";
+const MAX_LOG_LEN: usize = 1000;
 
 fn main() -> Result<()> {
     let mut args: Cli = Cli::from_args();
@@ -46,8 +53,8 @@ fn main() -> Result<()> {
         } else {
             pretty_env_logger::formatted_timed_builder()
         }
-        .filter(None, level.to_level_filter())
-        .init();
+            .filter(None, level.to_level_filter())
+            .init();
     }
 
     // Clean files
@@ -103,30 +110,94 @@ fn process_file(file: &Path) -> Result<()> {
     // Eg out/Chemistry
     let output_dir = format!("{}/{}", OUTPUT_DIR, stem);
     // Eg out/Chemistry/Chemistry.pdf
-    let generated_file = format!("{}/{}/{}.pdf", OUTPUT_DIR, stem, stem);
+    let generated_file = format!("{}/{}.pdf", output_dir, stem);
     // Eg: out/Chemistry.pdf
     let output_file = format!("{}/{}.pdf", OUTPUT_DIR, stem);
+    // Eg: out/Chemistry/Chemistry.fls
+    //let fls_file: PathBuf = format!("{}/{}.fls", output_dir, stem).into();
+
+    // FLSFile is just pointers to another string
+    // Declare the buffer here that it will live long enough
+    // We also need to give it something, as rust wont allow a reference to uninitialized reference
+    // even if it will always be overridden.
+    // This is cheep, as String::new doesn't allocate.
+    //let mut fls_buffer = std::io::Result::Ok(String::new());
+    //let fls = read_fls(fls_file, &mut fls_buffer);
 
     info!("Creating dir: {}", &output_dir);
     fs::create_dir_all(&output_dir)?;
 
-    //TODO: Figure out how many times to run
-    for _ in 0..3 {
-        debug!(
-            "Running {} -recorder -output-directory={} -halt-on-error {}",
-            LATEX_CMD, output_dir, output_file,
-        );
-        let output = Command::new(LATEX_CMD)
-            .arg("-recorder")
-            .arg(format!("-output-directory={}", output_dir))
-            .arg("-halt-on-error")
-            .arg(file)
-            .output()?;
+    let mut output = run_pdflatex(file, &output_dir).with_context(|| "Failed to invoke latex")?;
+
+    // TODO: Figure out exit status
+    handle_output_err(&output)?;
+
+    while run_again(&output) {
+        output = run_pdflatex(file, &output_dir).with_context(|| "Failed to invoke latex")?;
+        handle_output_err(&output)?;
     }
-    //println!("{}", std::str::from_utf8(&output.stdout)?);
 
     info!("Moving {} to {}", generated_file, output_file);
     fs::rename(generated_file, output_file)?;
 
     Ok(())
+}
+
+fn handle_output_err(output: &Output) -> Result<()> {
+    if !output.status.success() {
+        error!(
+            "{} failed to run, exit code {}",
+            LATEX_CMD,
+            output
+                .status
+                .code()
+                .map(|x| x.to_string())
+                .unwrap_or_else(|| "unknown".to_string()) // TODO: avoid unnecessary allocation
+        );
+        error!(
+            "{}",
+            String::from_utf8_lossy(
+                &output.stdout[output.stdout.len().saturating_sub(MAX_LOG_LEN)..]
+            )
+        );
+
+        Err(anyhow!("{} exited in failure", LATEX_CMD))
+    } else {
+        Ok(())
+    }
+}
+
+fn run_pdflatex(file: &Path, output_dir: &str) -> std::result::Result<Output, std::io::Error> {
+    // TODO: Make the debug and Command be the same
+    debug!(
+        "Running `{} -recorder -output-directory={} -halt-on-error {}`",
+        LATEX_CMD,
+        output_dir,
+        file.as_os_str().to_str().unwrap_or_else(|| {
+            warn!("Invalid utf8");
+            "<invalid utf8>"
+        }),
+    );
+    Command::new(LATEX_CMD)
+        .arg("-recorder")
+        .arg(format!("-output-directory={}", output_dir))
+        .arg("-halt-on-error")
+        .arg(file)
+        .output()
+}
+
+
+// TODO: Make this smarter
+// Clippy doesn't know their is no way to see if a [u8] contains a [u8] in std
+// Also regex has literal optimizations
+// Also it'l be easier when this is more complex
+#[allow(clippy::trivial_regex)]
+fn run_again(output: &Output) -> bool {
+    lazy_static! {
+
+        static ref MATCHES: Regex = Regex::new("Rerun").unwrap();
+    }
+
+    // TODO: Cache regex
+    MATCHES.is_match(&output.stdout)
 }
